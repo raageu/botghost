1594c33e5e64cdf55da1c428132cc24e6f67e945
raageu <160493000+raageu@users.noreply.github.com>
2024-02-19 13:10:54 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Używa Cardbota","Snajpi karty"];
 
 
 let currentIndex = 0;
